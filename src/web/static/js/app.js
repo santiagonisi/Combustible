424a8503a34b7fa20990855291bfc80375d67ed1
Santiago Enzo Nisi <santiagonisi5@gmail.com>
2026-05-04 13:43:08 -0300

    fix tamano boton imprimir igual al de eliminar

--- src/web/static/js/app.js
+++ src/web/static/js/app.js
@@ -85,7 +85,7 @@ function setVoucherRows(items) {
             <td>${quantityLabel}</td>
             <td>${voucher.fuel_type}</td>
             <td>${vehicle ? `${vehicle.code} / ${vehicle.plate}` : voucher.vehicle_id}</td>
-            <td style="text-align:center"><button type="button" onclick="window.open('/print/${voucher.id}','_blank')" title="Imprimir" style="margin:0;padding:4px 7px;background:#15803d;border:none;border-radius:8px;cursor:pointer;display:inline-flex;align-items:center;justify-content:center;color:#fff;"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 6 2 18 2 18 9"/><path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"/><rect x="6" y="14" width="12" height="8"/></svg></button></td>
+            <td style="text-align:center"><button type="button" class="table-action danger icon-btn" onclick="window.open('/print/${voucher.id}','_blank')" title="Imprimir" style="margin:0;background:#15803d;"><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="6 9 6 2 18 2 18 9"/><path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2"/><rect x="6" y="14" width="12" height="8"/></svg></button></td>
         `;
         vouchersTableBody.appendChild(tr);
     });
